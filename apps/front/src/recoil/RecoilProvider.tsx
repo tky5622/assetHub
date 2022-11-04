50ca8fff729698c32dc2ace85e3b630315ec49ac
takuya <>
2022-11-04 12:28:20 -0700

    add recoile

--- apps/front/src/recoil/RecoilProvider.tsx
+++ apps/front/src/recoil/RecoilProvider.tsx
@@ -1,8 +1,16 @@
-import { useCallback } from 'react';
+'use client'
+import { useCallback, useEffect, useState } from 'react';
 import { RecoilRoot, SetRecoilState } from 'recoil';
+import RecoilizeDebugger from 'recoilize';
 import { LensProfileIdState } from './atoms/LensProfile';
 import { LensSignupModalState } from './atoms/LensSignupModal';
 // import * as i18n from './atoms/i18n';
+// const RecoilizeDebugger = dynamic(
+//   () => {
+//     return import('recoilize');
+//   },
+//   { ssr: false }
+// );
 
 export default function RecoilProvider({
   children,
@@ -13,9 +21,22 @@ export default function RecoilProvider({
     set(LensSignupModalState, false);
     set(LensProfileIdState, '')
   }, []);
+  const [root, setRoot] = useState(null)
+
+  useEffect(() => {
+
+    if (typeof window.document !== 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      setRoot(document.getElementById('__next'));
+    }
+  }, [root]);
+
+
 
   return (
     <RecoilRoot initializeState={initializeState}>
+      <RecoilizeDebugger root={root}/>
       {children}
     </RecoilRoot>
   )
