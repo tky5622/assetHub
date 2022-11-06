01f4090cfc713d017a584fc35bbdec5a5482717a
takuya <>
2022-11-06 01:32:59 -0800

    fix typed errors

--- apps/front/src/recoil/DebugObserver.tsx
+++ apps/front/src/recoil/DebugObserver.tsx
@@ -7,6 +7,8 @@ export const DebugObserver = () => {
   const snapshot = useRecoilSnapshot()
   React.useEffect(() => {
     console.debug('The following atoms were modified:')
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     for (const node of snapshot.getNodes_UNSTABLE({ isModified: true })) {
       console.debug(node.key, snapshot.getLoadable(node))
     }
